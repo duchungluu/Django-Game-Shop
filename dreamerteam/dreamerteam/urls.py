e24817a82464a9e37eff2ac2d61b3417bc5309ba
Teemu Huovinen <teemu.huovinen@hotmail.fi>
2016-01-12 13:55:52 +0200

    redirect to webshop-app by default

--- dreamerteam/dreamerteam/urls.py
+++ dreamerteam/dreamerteam/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^webshop/', include('webshop.urls')),
+    url(r'^$', include('webshop.urls')),
     url(r'^admin/', admin.site.urls),
 ]
