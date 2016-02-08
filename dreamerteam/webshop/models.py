bbe847142549dc98179a8b83f8e73f583986e4b5
Mehrad Mohammadi <mohammm4@aalto.fi>
2016-02-08 15:41:52 +0200

    new debugging

--- dreamerteam/webshop/models.py
+++ dreamerteam/webshop/models.py
@@ -3,18 +3,6 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 import datetime
 
-class Game(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.FloatField()
-    url = models.URLField()
-    published = models.DateTimeField('date published')
-    description = models.TextField()
-    developer = models.ForeignKey(User, on_delete=models.CASCADE,
-        related_name='developed_games', unique=False,
-        limit_choices_to= {'groups__pk': 1})
-
-    def __str__(self):
-        return "{} ({})".format(self.name, self.price)
 
 
 #class Developer(User):
@@ -31,11 +19,25 @@ class UserProfile(models.Model):
     key_expires = models.DateTimeField(default=timezone.now)
     isDeveloper = models.BooleanField(default = False)
     username = models.CharField(max_length=40)
+
     def __str__(self):
         return self.user.username
 
     class Meta:
         verbose_name_plural='User profiles'
+
+class Game(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    url = models.URLField()
+    published = models.DateTimeField('date published')
+    description = models.TextField()
+    developer = models.ForeignKey(UserProfile, on_delete=models.CASCADE,
+        related_name='developed_games', unique=False,
+        limit_choices_to= {'groups__pk': 1})
+
+    def __str__(self):
+        return "{} ({})".format(self.name, self.price)
 
 
 class Transaction(models.Model):
