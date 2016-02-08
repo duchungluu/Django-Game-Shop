f2d2ab4fded35822637e94cde1ef61de359d9504
Mehrad Mohammadi <mohammm4@aalto.fi>
2016-02-08 15:09:48 +0200

    one class of the user exists now instead of two

--- dreamerteam/webshop/models.py
+++ dreamerteam/webshop/models.py
@@ -18,33 +18,35 @@ class Game(models.Model):
     def __str__(self):
         return "{} ({})".format(self.name, self.price)
 
+
+#class Developer(User):
+#    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+#    games = models.ManyToManyField(Game, related_name='developers')
+
+#class Customer(User):
+#    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+#    games = models.ManyToManyField(Game, related_name='customers')
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    activation_key = models.CharField(max_length=40, blank=True)
+    key_expires = models.DateTimeField(default=timezone.now)
+    isDeveloper = models.BooleanField(default = False)
+    username = models.CharField(max_length=40)
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name_plural='User profiles'
+
+
 class Transaction(models.Model):
 
     game = models.ForeignKey(Game)
-    buyer = models.ForeignKey(User, related_name='bought_games')
+    buyer = models.ForeignKey(UserProfile, related_name='bought_games')
     state = models.CharField(blank=True, max_length=200)
     buy_started = models.DateTimeField(default=datetime.datetime.now, blank=True)
     buy_completed = models.DateTimeField(null=True)
 
     def __str__(self):
         return "{}: {}".format(self.buyer, self.game)
-
-class Developer(User):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    games = models.ManyToManyField(Game, related_name='developers')
-
-class Customer(User):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    games = models.ManyToManyField(Game, related_name='customers')
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    activation_key = models.CharField(max_length=40, blank=True)
-    key_expires = models.DateTimeField(default=timezone.now)
-
-
-    def __str__(self):
-        return self.user.username
-
-    class Meta:
-        verbose_name_plural=u'User profiles'
