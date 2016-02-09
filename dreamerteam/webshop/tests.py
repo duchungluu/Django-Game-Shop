a4ef5f7db829e50b2e0e0cf77a7ac7759cfb41cb
Teemu Huovinen <teemu.huovinen@hotmail.fi>
2016-02-09 19:29:13 +0200

    setting up tests

--- dreamerteam/webshop/tests.py
+++ dreamerteam/webshop/tests.py
@@ -1,6 +1,11 @@
 from django.test import TestCase, Client
+from django.utils import timezone
+from django.core import mail
 from django.core.urlresolvers import reverse
 from django.template.loader import render_to_string
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User, Group
+from webshop.forms import RegistrationForm, GameForm
 from webshop.models import *
 
 class TemplateTestCase(TestCase):
@@ -8,8 +13,39 @@ class TemplateTestCase(TestCase):
     def setUp(self):
         self.client = Client()
 
+        devGroup = Group.objects.create(name='Developer')
+        cusGroup = Group.objects.create(name='Customer')
+
+        userDeveloper = User.objects.create_user('developer',
+            'developer@gmail.com', 'developer')
+        userCustomer = User.objects.create_user('customer',
+            'customer@gmail.com', 'customer')
+
+        devProfile = UserProfile.objects.create(user=userDeveloper,
+            isDeveloper = True, username = 'developer')
+        UserProfile.objects.create(user=userCustomer,
+            isDeveloper = False, username = 'customer')
+
+        Game.objects.create(name = 'Test Game',
+        price = 0.99, url = 'www.google.fi', published = timezone.now(),
+        description = 'Test Description', developer = devProfile)
+
+        devGroup.user_set.add(userDeveloper)
+        cusGroup.user_set.add(userCustomer)
+
+    def testLogin(self):
+        """ Check that temp users work """
+
+        self.client.login(username='developer', password='developer')
+        devUser = User.objects.get(username='developer')
+        profile = UserProfile.objects.get(user=devUser)
+        print ('\nDeveloper correctly set up:',end=" ")
+        print (profile.isDeveloper)
+        self.client.logout()
+
     def testInvalidUrls(self):
         """ Test for invalid urls """
+        print ('Testing invalid urls')
 
         res = self.client.get('/game/xx')
         self.assertEquals(res.status_code, 404, "Requesting a game page with an invalid code.")
@@ -23,8 +59,9 @@ class TemplateTestCase(TestCase):
         res = self.client.get('/xx')
         self.assertEquals(res.status_code, 404, "Requesting a page with an invalid code.")
 
-    def testContinentMenu(self):
+    def testGameMenu(self):
         """ Tests that the game list is rendered correctly """
+        print('Testing gamelist')
 
         all_games = ({'name': 'testGame', 'description': 'testScription', 'price' : 0.99},
         {'name': 'bestGame', 'description': 'badScription', 'price' : 1.99})
@@ -37,10 +74,18 @@ class TemplateTestCase(TestCase):
             self.assertTrue(ren.find(str(game['price'])) > -1,
             "Testing if the rendered menu contains correct game price")
 
-    # Not yet implemented
-    # def testPageContents(self):
-    #     """ This test requests pages for all games
-    #     TODO: Check contents of page """
-    #     for game in Game.objects.all():
-    #         response = self.client.get('game/' + game.id), HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-    #         self.assertEquals(response.status_code, 200, "Testing request status code.")
+
+    def testBuyGame(self):
+        """ Test buying game by accessing /buy/success """
+        print('\nTesting access to /buy/success')
+
+        # No user
+        self.client.logout()
+        res = self.client.get('/buy/success/1')
+        self.assertTemplateUsed(res, 'webshop/buy_error.html')
+
+        # With user
+        self.client.login(username='customer', password='customer')
+        res = self.client.get('/buy/success/1')
+        self.assertTemplateUsed(res, 'webshop/buy_error.html')
+        self.client.logout()
